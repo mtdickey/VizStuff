2d9659882649ed5ed5398818a48e835d4f058788
Michael Dickey <mtdickey@ncsu.edu>
2016-10-24 16:09:18 -0400

    Added Dalton to QB-WR Tandem

--- QB-WR-Tandem-Viz.R
+++ QB-WR-Tandem-Viz.R
@@ -14,6 +14,7 @@ Stafford <- read.csv( "StaffordTDs.csv", stringsAsFactors = F, header = F)
 Brees    <-  read.csv("BreesTDs.csv", stringsAsFactors = F, header = F)
 Romo     <- read.csv( "RomoTDs.csv", stringsAsFactors = F, header = F)
 Rodgers  <-  read.csv("RodgersTDs.csv", stringsAsFactors = F, header = F)
+Dalton <- read.csv("DaltonTDs.csv", stringsAsFactors = F, header = F)
 
 # Passer Identifier columns
 Rivers$Passer <- "Rivers"
@@ -24,6 +25,7 @@ Stafford$Passer <- "Stafford"
 Brees$Passer <- "Brees"
 Romo$Passer <- "Romo"
 Rodgers$Passer <- "Rodgers"
+Dalton$Passer <- "Dalton"
 
 ## Name columns consistently
 colnames(Rivers)[1:2] <- c("Receiver", "TotalTDs")
@@ -34,9 +36,10 @@ colnames(Stafford)[1:2] <- c("Receiver", "TotalTDs")
 colnames(Brees)[1:2] <- c("Receiver", "TotalTDs")
 colnames(Romo)[1:2] <- c("Receiver", "TotalTDs")
 colnames(Rodgers)[1:2] <- c("Receiver", "TotalTDs")
+colnames(Dalton)[1:2] <- c("Receiver", "TotalTDs")
 
 # Combine
-All <- rbind(Brady, Rivers, Manning, Ryan, Stafford, Brees, Romo, Rodgers)
+All <- rbind(Brady, Rivers, Manning, Ryan, Stafford, Brees, Romo, Rodgers, Dalton)
 
 
 ## Variables for labeling
@@ -47,6 +50,7 @@ All$together[which(All$Passer == "Brady" &   All$ReceiverLastName %in% c("Gronko
 All$together[which(All$Passer == "Brady" & !(All$ReceiverLastName %in% c("Gronkowski", "Edelman")))] <- "No"
 All$together[which(All$Passer == "Rivers" &  All$ReceiverLastName %in% c("Gates", "Allen"))] <- "Yes"
 All$together[which(All$Passer == "Rivers" &!(All$ReceiverLastName %in% c("Gates", "Allen")))] <- "No"
+All$together[which(All$Passer == "Dalton")] <- "Yes"
 All$together[which(All$Passer == "Manning")] <- "No"
 All$together[which(All$Passer == "Brees")] <- "No"
 All$together[which(All$Passer == "Stafford")] <- "No"
@@ -76,7 +80,7 @@ viz <- All %>%
   geom_violin(alpha = .8) + geom_boxplot(width=0.1, fill="white") + 
   geom_text(aes(label = outlier, colour = factor(together)), na.rm = T, hjust = 1.1, size = 3.5) +
   scale_colour_discrete(l=40) +
-  scale_fill_manual(values=c("#3182bd", "#b0e0e6", "#cfb53b", "#0D254C", "#006DB0", "#BD0D18", "#0D254C", "#203731")) +
+  scale_fill_manual(values=c("#3182bd", "#b0e0e6", "#cfb53b", "#0D254C", "#006DB0", "#BD0D18", "#0D254C", "#203731", "#FB4F14")) +
   guides(fill = F) +
   labs(x = "QB", y = "TDs Thrown to Receiver", title = "Dynamic Duos", colour = "Active Tandem")
 
